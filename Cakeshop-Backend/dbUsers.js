a28a50e0db209a63da85c566f07d2e6df732cac2
Anjali Goyal <anjali24.7goyal@gmail.com>
2021-05-04 02:02:17 +0530

    Login done, signup improvements, partial uploads done

--- Cakeshop-Backend/dbUsers.js
+++ Cakeshop-Backend/dbUsers.js
@@ -2,10 +2,31 @@ import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
+const validateEmail = (email) => {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+};
+
+const validatePassword = (password) => {
+  const re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+  return re.test(String(password));
+};
+
 const usersSchema = Schema({
-  email: String,
-  name: String,
-  password: String,
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    validate: [validateEmail, "Please fill a valid email address"],
+  },
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    validate: [validatePassword, "Please fill a valid password"],
+  },
   token: String,
 });
 
--- Cakeshop-Backend/server.js
+++ Cakeshop-Backend/server.js
@@ -2,15 +2,29 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-
+// import cloudinary from "./utils/cloudinary";
+// import upload from "./utils/multer";
 import Users from "./dbUsers.js";
+import fileupload from "express-fileupload";
+import cloudinary from "cloudinary";
 
 const app = express();
 const port = process.env.PORT || 9000;
 
 app.use(express.json());
 app.use(cors());
+app.use(
+  fileupload({
+    useTempFiles: true,
+  })
+);
 dotenv.config();
+
+cloudinary.v2.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
 
 const connection_url = `mongodb+srv://anjali:${process.env.PASSWORD}@cluster0.4k5gg.mongodb.net/cakeshopdb?retryWrites=true&w=majority`;
 
@@ -50,14 +64,14 @@ app.post("/api/register", (req, res) => {
       res.status(500).send(err);
     } else {
       if (count !== 0) {
-        res.status(409).send("User already exists");
+        res.status(200).send({ message: "User already exists" });
       } else {
         userData.token = token();
         Users.create(userData, (err, data) => {
           if (err) {
             res.status(500).send(err);
           } else {
-            res.status(201).send("User registered successfully");
+            res.status(201).send({ message: "User registered successfully" });
           }
         });
       }
@@ -65,8 +79,41 @@ app.post("/api/register", (req, res) => {
   });
 });
 
-// app.post("/api/login", (req, res) => {
-//   console.log(req.body);
-// });
+app.post("/api/login", (req, res) => {
+  const userData = req.body;
+
+  Users.find(
+    { email: userData.email, password: userData.password },
+    (err, data) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        if (Object.keys(data).length === 0) {
+          res.status(200).send({ message: "Invalid Credentials" });
+        } else {
+          res.status(200).send({
+            email: data[0].email,
+            name: data[0].name,
+            token: data[0].token,
+          });
+        }
+      }
+    }
+  );
+});
+
+app.post("/api/upload", (req, res) => {
+  const file = req.files.photo;
+  // console.log(file);
+
+  // save image from temp file location to the server
+  cloudinary.v2.uploader.upload(file.tempFilePath, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send({ imageUrl: data.url });
+    }
+  });
+});
 
 app.listen(port, () => console.log(`Listening on localhost:${port}`));
